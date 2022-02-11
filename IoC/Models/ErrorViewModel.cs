40c25e8e9c3d69be7420221f17beb4473258c15f
= <=>
2022-02-11 16:18:23 -0700

    init commit

---
+++ IoC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IoC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
